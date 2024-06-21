729073f1912ea7076f7d949f5e80305119065525
Marcelo Pignataro <pignataromd@gmail.com>
2024-06-21 16:10:08 +0100

    Adds ping_striga file to test the access to the striga APIs infrasctructure

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,8 @@ import * as serviceAccount from "./serviceAccount.json";
 import newUser from "./create_user";
 import newStrigaUser from "./striga/create_striga_user";
 import requestOnetimePassword from "./request_onetime_password";
-import pingStriga from "./ping_striga";
 import { FIREBASE_DATABASE_URL } from "./keys";
+import pingStriga from "./striga/ping_striga";
 import verifyOnetimePassword from "./verify_onetime_password";
 
 admin.initializeApp({
