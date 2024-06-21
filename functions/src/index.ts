ac1743562e09e2c255b58bbceafeb7c817539f36
Marcelo Pignataro <pignataromd@gmail.com>
2024-06-21 16:08:16 +0100

    Adds the newStrigaUser to the index file.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import * as serviceAccount from "./serviceAccount.json";
 import newUser from "./create_user";
-import newStrigaUser from "./create_striga_user";
+import newStrigaUser from "./striga/create_striga_user";
 import requestOnetimePassword from "./request_onetime_password";
 import pingStriga from "./ping_striga";
 import { FIREBASE_DATABASE_URL } from "./keys";
