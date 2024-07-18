d5088c8b9c3e512c6a902003ff839da5b1bfc930
Marcelo Pignataro <pignataromd@gmail.com>
2024-07-18 18:28:35 +0100

    Updates imports and reorganizes the order of APIs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,42 +8,37 @@ import { addPhoneToUserByEmail } from "./update_user";
 import newStrigaUser from "./striga/create_striga_user";
 import pingStriga from "./striga/ping_striga";
 
-import requestOnetimePassword from "./totp/request_onetime_password";
-import verifyOnetimePassword from "./totp/verify_onetime_password";
-import verifyEmailOnetimePassword from "./totp/verify_email_onetime_password";
-import requestEmailOnetimePassword from "./totp/request_email_onetime_password";
-import authenticateUsingOnetimePassword from "./totp/authenticate_using_onetime_password";
-//
+import authenticateUsingOnetimePassword from "./auth/authenticate_using_onetime_password";
+import requestEmailOnetimePassword from "./validation/request_email_onetime_password";
+import requestOnetimePassword from "./validation/request_onetime_password";
+import verifyOnetimePassword from "./validation/verify_onetime_password";
+import verifyEmailOnetimePassword from "./validation/verify_email_onetime_password";
+
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 	databaseURL: functions.config().fbase.fbase_db_url,
 });
-// test striga APIs
+// striga APIs
 export const handshakeStriga = functions.https.onRequest(pingStriga);
-// create user
+export const createStrigaUser = functions.https.onRequest(newStrigaUser);
+// user creation
 export const createUserByPhone = functions.https.onRequest(newUserByPhone);
 export const createUserByEmail = functions.https.onRequest(newUserByEmail);
-export const createStrigaUser = functions.https.onRequest(newStrigaUser);
-// phone validation
+// phone & email validation
 export const getOneTimePassword = functions.https.onRequest(
 	requestOnetimePassword
 );
 export const checkOneTimePassword = functions.https.onRequest(
 	verifyOnetimePassword
 );
-// email validation
 export const getEmailOneTimePassword = functions.https.onRequest(
 	requestEmailOnetimePassword
 );
 export const checkEmailOneTimePassword = functions.https.onRequest(
 	verifyEmailOnetimePassword
 );
-// phone and email authentication
+// phone & email authentication
 export const authUsingOneTimePassword = functions.https.onRequest(
 	authenticateUsingOnetimePassword
 );
-// export const authEmailhUsingOneTimePassword = functions.https.onRequest(
-// 	() => {}
-// );
-//updates
 export const addPhoneToUser = functions.https.onRequest(addPhoneToUserByEmail);
